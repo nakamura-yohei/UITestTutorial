881b49ba651c29f14fbf2c471bca8cecb4f3604d
yohei-nakamura <nakamura.yohei@fixer.co.jp>
2018-07-20 11:16:21 +0900

    [追加] ボタンのアクションを追加

--- UITestTutorial/ViewController.swift
+++ UITestTutorial/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func touchUpButton1(_ sender: Any) {
+        myLabel.text = "Button1 touch upped"
+    }
+    
+    @IBAction func touchUpButton2(_ sender: Any) {
+        myLabel.text = "Button2 touch upped"
+    }
+    
 }
 
